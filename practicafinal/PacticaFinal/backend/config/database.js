72d353094d4f180ca2ede3a32fe7fb0b365e8af4
LuisGamboaS <63593597+legs30011@users.noreply.github.com>
2025-03-19 13:11:40 -0300

    Delete practicafinal/PacticaFinal directory

--- practicafinal/PacticaFinal/backend/config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("books_db", "postgres", "1234", {
-  host: "localhost",
-  port: 5432,
-  dialect: "postgres",
-});
-
-module.exports = sequelize;
